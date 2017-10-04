b600d8c53475fe1037889593f917053a1561aba1
dugls <rgasiy12@gmail.com>
2017-10-04 20:32:04 +0300

    Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LettersComponent } from './letters/index';
 import { DataService } from './data.service'
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/letters/letters.component.ts
+++ src/app/letters/letters.component.ts
@@ -9,6 +9,8 @@
  import { DataService } from "../data.service"; 
  import { Goals } from "../goals";
 
+ 
+
 @Component({
   selector: 'app-letters',
   templateUrl: './letters.component.html',
@@ -19,9 +21,30 @@
   			 transform: 'scale(1)'
   		})),
   		state('active', style({
-  			transform: 'translateX(550px)'
+  			transform: 'translateX(535px)'
   		})),
-  		transition('inactive=>active', animate('100ms ease-in')), 
+      
+  		transition('inactive=>active', animate('600ms', keyframes([
+        style({
+        transform: 'translate3d(0px, 0, 0)'
+      }),style({
+         opacity: 1,
+         transform: 'translate3d(325px, 0, 0)'
+      }),style({
+        transform: 'translate3d(420px, 0, 0)'
+      }),style({
+        transform: 'translate3d(550px, 0, 0)'
+      }),style({
+        transform: 'translate3d(525px, 0, 0)'
+      }),style({
+        transform: 'translate3d(540px, 0, 0)'
+      }),style({
+        transform: 'translate3d(525px, 0, 0)'
+      }),style({
+        transform: 'translate3d(535px, 0, 0)'
+      }),
+
+        ]))), 
   		transition('active=>inactive', animate('100ms ease-out')), 
   		]),  
   	trigger('inputPanel', [
@@ -31,12 +54,21 @@
   		state('visible', style({
   			transform: 'scale(1)'
   		})),
-  		transition('visible=>invisible', animate('100ms ease-in')), 
-  		transition('invisible=>visible', animate('100ms ease-out')), 
+  		transition('visible=>invisible', animate('400ms', keyframes([
+         style({
+          oopacity: 1
+      }),style({
+          opacity: 0,
+          transform: 'scale3d(.3, .3, .3)'
+      }),style({
+          opacity: 0
+      })
+  ]))), 
+  		transition('invisible=>visible', animate('100ms')), 
   		]), 
-  			trigger('hideInput', [
+  	trigger('hideInput', [
   					state('hide', style({
-  						transform: 'scale(0)'
+ 						transform: 'scale(0)'
   					})),
   					state('show', style({
   						transform: 'scale(1)'
@@ -44,15 +76,27 @@
   					transition('hide=>show', animate('250ms ease-in') ),
   			
   				]),
-
-
+    trigger('letterDescription', [
+            state('no', style({
+              transform: 'scale(0)'
+            })),
+            state('yes', style({
+              transform: 'scale(1)'
+            })),
+            transition('yes=>no', animate('100ms ease-in')),
+            transition('no=>yes', animate('100ms ease-out')),
+      ])
   ]
 })
 export class LettersComponent implements OnInit {
+
+  /*Animations*/
 		state: string = 'inactive';
 		inp: string = 'visible';
 		hid: string = 'hide';
-		
+    desc: string = 'yes';
+
+
 	toggleMove(){
 		this.state = (this.state === 'inactive' ? 'active': 'inactive');
 }
@@ -63,9 +107,16 @@ export class LettersComponent implements OnInit {
 	showInput(){
 		this.hid = (this.hid === 'hide' ? 'show' : 'hide')
 	}
+  showDescription(){
+    this.desc = (this.desc === 'yes' ? 'no' : 'yes')
+  }
+  /*Animations*/
 
   goals: Goals[] = [];
   goal: string;
+
+  
+
 
   constructor(private dataService: DataService) { 
       this.dataService.getGoals()
@@ -73,6 +124,8 @@ export class LettersComponent implements OnInit {
         this.goals = goals;
       })
   }
+
+
 
   addGoal(event){
       event.preventDefault();
@@ -100,7 +153,12 @@ export class LettersComponent implements OnInit {
   }
 
 
+
+
+///SVG
+
   ngOnInit() {
+    
   }
- 
+
 }
